21b37e944689f31d8879d977a267faa078c05d4e
Max Neklyudov <macscomp@gmail.com>
2012-06-07 18:42:39 +0400

    Add build script

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+
+ROOT=`pwd`
+TOOLCHAIN_BIN="$ROOT/../toolchain/uclinux/arm-2011.03/bin"
+UBOOT_TOOLS_BIN="$ROOT/../u-boot-uwic/tools"
+CC1_PATH="$ROOT/../toolchain/uclinux/arm-2011.03/libexec/gcc/arm-uclinuxeabi/4.5.2/"
+export PATH=$TOOLCHAIN_BIN:$UBOOT_TOOLS_BIN:$CC1_PATH:$PATH
+
+#copy firmware
+#cp rt73.bin firmware/
+
+make clean CROSS_COMPILE=arm-uclinuxeabi-
+make V=1 CROSS_COMPILE=arm-uclinuxeabi- || exit 1
+
+echo == Compiled ==
+
+#./deploy.sh
