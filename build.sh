eba7cb2b14ac9a0ad031a37b875e009161728e5e
Max Neklyudov <macscomp@gmail.com>
2012-08-28 14:06:46 +0400

    Remove obsolite code from build.sh

--- build.sh
+++ build.sh
@@ -7,9 +7,6 @@ UBOOT_TOOLS_BIN="$ROOT/../u-boot-uwic/tools"
 CC1_PATH="$ROOT/../toolchain/uclinux/arm-2011.03/libexec/gcc/arm-uclinuxeabi/4.5.2/"
 export PATH=$TOOLCHAIN_BIN:$UBOOT_TOOLS_BIN:$CC1_PATH:$PATH
 
-#copy firmware
-#cp rt73.bin firmware/
-
 if [[ "$1" = "rebuild" ]]; then
   make clean CROSS_COMPILE=arm-uclinuxeabi-
 fi
@@ -17,5 +14,3 @@ fi
 make CROSS_COMPILE=arm-uclinuxeabi- || exit 1
 
 echo == Compiled ==
-
-#./deploy.sh
