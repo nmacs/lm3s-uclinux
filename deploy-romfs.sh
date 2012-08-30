42b4931cef78f85d991d0ed8fc2f7ebd73f975a7
Max Neklyudov <macscomp@gmail.com>
2012-08-30 13:24:37 +0400

    Deploy all content of romfs

--- deploy-romfs.sh
+++ deploy-romfs.sh
@@ -2,17 +2,9 @@
 
 ROOT=`pwd`
 
-deploy_list=( bin etc lib )
-romfs="$ROOT/romfs"
-
 rm -rf /nfsroot/*
 
-for item in "${deploy_list[@]}"; do
-  rm -rf /nfsroot/$item
-  cp -r $romfs/$item /nfsroot/
-done
-
-cp -r $ROOT/romfs /nfsroot/
+cp -r $ROOT/romfs/* /nfsroot/
 
 find $ROOT/user/mtd-utils/ubi-utils -executable -type f -not -name '*.gdb' | xargs -I {} cp {} /nfsroot/bin
 
