2d45eaed702d8a0290ec6df7c45065a59cce6b3d
Max Neklyudov <macscomp@gmail.com>
2012-08-30 13:17:52 +0400

    Polulate /nfsroot/dev from ext2 image of romfs

--- deploy-romfs.sh
+++ deploy-romfs.sh
@@ -5,6 +5,8 @@ ROOT=`pwd`
 deploy_list=( bin etc lib )
 romfs="$ROOT/romfs"
 
+rm -rf /nfsroot/*
+
 for item in "${deploy_list[@]}"; do
   rm -rf /nfsroot/$item
   cp -r $romfs/$item /nfsroot/
@@ -13,3 +15,12 @@ done
 cp -r $ROOT/romfs /nfsroot/
 
 find $ROOT/user/mtd-utils/ubi-utils -executable -type f -not -name '*.gdb' | xargs -I {} cp {} /nfsroot/bin
+
+cp -r $ROOT/images /nfsroot/
+
+mkdir -p /tmp/uwic-image
+mount $ROOT/images/Image.ext2 /tmp/uwic-image
+cp -r /tmp/uwic-image/dev /nfsroot/
+umount /tmp/uwic-image
+
+
