ce6a73b06d8167cd01b6158c053d6abbb4a311a0
Max Neklyudov <macscomp@gmail.com>
2012-08-31 09:15:01 +0400

    Add local uwic-jtag.cfg

--- load-kernel.sh
+++ load-kernel.sh
@@ -4,6 +4,6 @@ ROOT=`pwd`
 image="$ROOT/romfs/boot/linux.bin"
 #load_addr="0x60400000"
 
-openocd -f "$ROOT/../toolchain/jtag/uwic.cfg" -c "load_sdram $image" || exit 1
+openocd -f "$ROOT/vendors/Elster/uwic/uwic-jtag.cfg" -c "load_sdram $image" || exit 1
 
 echo == Image loaded ==
