86bcbe7138cb240a87e2ba1f2f9ffcd3c26d64fc
Max Neklyudov <macscomp@gmail.com>
2012-10-02 14:23:14 +0400

    Add TOOLCHAIN variable in build.sh

--- build.sh
+++ build.sh
@@ -2,15 +2,16 @@
 
 
 ROOT=`pwd`
-TOOLCHAIN_BIN="$ROOT/../toolchain/uclinux/arm-2011.03/bin"
+TOOLCHAIN="$ROOT/../toolchain/uclinux/arm-2011.03"
+TOOLCHAIN_BIN="$TOOLCHAIN/bin"
 UBOOT_TOOLS_BIN="$ROOT/../u-boot-uwic/tools"
-CC1_PATH="$ROOT/../toolchain/uclinux/arm-2011.03/libexec/gcc/arm-uclinuxeabi/4.5.2/"
+CC1_PATH="$TOOLCHAIN/libexec/gcc/arm-uclinuxeabi/4.5.2/"
 export PATH=$TOOLCHAIN_BIN:$UBOOT_TOOLS_BIN:$CC1_PATH:$PATH
 
 if [[ "$1" = "rebuild" ]]; then
   make clean CROSS_COMPILE=arm-uclinuxeabi-
 fi
 
-make CROSS_COMPILE=arm-uclinuxeabi- || exit 1
+make TOOLCHAIN=$TOOLCHAIN CROSS_COMPILE=arm-uclinuxeabi- || exit 1
 
 echo == Compiled ==
