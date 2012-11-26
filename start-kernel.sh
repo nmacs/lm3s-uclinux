48b102bfbaa9300a0ffe3a00a0576581ac9df9dc
Max Neklyudov <macscomp@gmail.com>
2012-11-26 23:19:12 +0400

    Improve start-kernel.sh

--- start-kernel.sh
+++ start-kernel.sh
@@ -7,3 +7,13 @@ image="$ROOT/romfs/boot/linux.bin"
 openocd -f "$ROOT/vendors/Elster/uwic/uwic-jtag.cfg" -c "start_kernel $image" || exit 1
 
 echo == Image loaded ==
+
+tty=$1
+
+echo == Starting kernel... ==
+echo -n 1 > "$tty"
+sleep 1
+echo setenv bootargs > "$tty"
+echo bootm 0x60400000 > "$tty"
+
+echo == Started ==
