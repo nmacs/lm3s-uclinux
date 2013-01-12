55b97fd47f1416540951fcadecc4456c452e7d53
Max Neklyudov <macscomp@gmail.com>
2013-01-12 10:19:36 +0400

    Clean romfs dir befor building

--- build.sh
+++ build.sh
@@ -8,6 +8,8 @@ UBOOT_TOOLS_BIN="$ROOT/../u-boot-uwic/tools"
 CC1_PATH="$TOOLCHAIN/libexec/gcc/arm-uclinuxeabi/4.5.2/"
 export PATH=$TOOLCHAIN_BIN:$UBOOT_TOOLS_BIN:$CC1_PATH:$PATH
 
+rm -rf ./romfs
+
 if [[ "$1" = "rebuild" ]]; then
   make clean CROSS_COMPILE=arm-uclinuxeabi-
 fi
