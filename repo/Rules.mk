7d20ce52e82a8238e9b353933aaa33519363bbec
Max Neklyudov <macscomp@gmail.com>
2013-01-19 08:38:18 +0400

    Repack deb to ucdeb

--- repo/Rules.mk
+++ repo/Rules.mk
@@ -6,9 +6,19 @@ repo: content
 	cp control content/DEBIAN/control
 	version=`cat control | grep Version: | cut -d' ' -f 2`; \
 	name=`cat control | grep Package: | cut -d' ' -f 2`; \
-	filename="$$name-$$version.deb"; \
+	filename="$$name-$$version.ucdeb"; \
 	dpkg --build content "$$filename"; \
+	rm -rf .tmp; \
+	mkdir -p .tmp; \
+	mv "$$filename" ./.tmp; \
+	cd ./.tmp; \
+	ar -x "$$filename"; \
+	rm -f "$$filename"; \
+	gunzip data.tar.gz; \
+	gunzip control.tar.gz; \
+	ar rcs "$$filename" control.tar data.tar; \
 	mv "$$filename" $$REPODIR/output/; \
+	cd ..; \
 	echo "Filename: $$filename" >> content/DEBIAN/control; \
 	cat control >> $$REPODIR/output/Packages; \
 	echo "" >> $$REPODIR/output/Packages
