780c32f1c2ae16435975b24d221821aac0dbc79d
Max Neklyudov <macscomp@gmail.com>
2013-01-20 17:18:53 +0400

    Fix cosocket.lua

--- lib/liblua/copas-1.1.6/tests/cosocket.lua
+++ lib/liblua/copas-1.1.6/tests/cosocket.lua
@@ -1,11 +1,13 @@
 -------------------------------------------------------------------------------
 -- Copas - Coroutine Oriented Portable Asynchronous Services
--- 
+--
 -- Copas Wrapper for socket.http module
--- 
+--
 -- Written by Leonardo Godinho da Cunha
 -------------------------------------------------------------------------------
-require "copas"
+local copas = require("copas")
+local socket = require ("socket")
+local base = _G
 
 module "copas.cosocket"
 
@@ -20,10 +22,10 @@ function tcp ()
 	skt=socket.tcp()
 	w_skt_mt={__index = skt
 	}
-	ret_skt = setmetatable ({socket = skt}, w_skt_mt)
+	ret_skt = base.setmetatable ({socket = skt}, w_skt_mt)
 	ret_skt.settimeout = function (self,val)
-				return self.socket:settimeout (val) 
-			end 	
+				return self.socket:settimeout (val)
+			end
 	ret_skt.connect = function (self,host, port)
 				ret,err = copas.connect (self.socket,host, port)
 				local d = copas.wrap(self.socket)
@@ -32,12 +34,23 @@ function tcp ()
 					local ret,val=d.send(client, data)
 					return ret,val
 				end
-    				self.receive=d.receive
-    				self.close = function (w_socket)
-    					ret=w_socket.socket:close()
-    					return ret
-    				end
-				return ret,err
-			end 
+				self.receive= function(client, pattern, prefix)
+					local ret, err = d.receive(client, pattern, prefix)
+					if not ret then
+						return nil, err
+					end
+
+					if prefix then
+						return prefix .. ret
+					else
+						return ret
+					end
+				end
+				self.close = function (w_socket)
+					ret=w_socket.socket:close()
+					return ret
+				end
+				return ret, err
+			end
 	return  ret_skt
 end
