1416f89400a3ccefa851696896a9d93704ba538a
Max Neklyudov <macscomp@gmail.com>
2013-04-09 10:19:44 +0400

    Split repo output to deb and ucdeb directories

--- repo/Rules.mk
+++ repo/Rules.mk
@@ -4,11 +4,11 @@ include $(ROOTDIR)/vendors/config/common/config.arch
 
 repo: content
 	mkdir -p content/DEBIAN
-	cp control content/DEBIAN/control
 	version=`cat control | grep Version: | cut -d' ' -f 2`; \
 	name=`cat control | grep Package: | cut -d' ' -f 2`; \
 	deb="$$name-$$version.deb"; \
 	ucdeb="$$name-$$version.ucdeb"; \
+	cp control content/DEBIAN/control; \
 	echo "Filename: $$ucdeb" >> content/DEBIAN/control; \
 	dpkg --build content "$$deb"; \
 	rm -rf .tmp; \
@@ -20,13 +20,20 @@ repo: content
 	gunzip data.tar.gz; \
 	gunzip control.tar.gz; \
 	ar rcs "$$ucdeb" control.tar data.tar; \
-	mv "$$ucdeb" "$$REPODIR/output/"; \
+	mv "$$ucdeb" "$$REPODIR/output/ucdeb"; \
 	cd ..; \
-	mv "$$deb" "$$REPODIR/output/"; \
-	cat content/DEBIAN/control >> $$REPODIR/output/Packages; \
-	md5=`md5sum "$$REPODIR/output/$$ucdeb" | cut -d' ' -f 1`; \
-	echo "MD5Sum: $$md5" >> $$REPODIR/output/Packages; \
-	echo "" >> $$REPODIR/output/Packages
+	cat content/DEBIAN/control >> $$REPODIR/output/ucdeb/Packages; \
+	md5=`md5sum "$$REPODIR/output/ucdeb/$$ucdeb" | cut -d' ' -f 1`; \
+	echo "MD5Sum: $$md5" >> $$REPODIR/output/ucdeb/Packages; \
+	echo "" >> $$REPODIR/output/ucdeb/Packages; \
+	cp control content/DEBIAN/control ; \
+	echo "Filename: $$deb" >> content/DEBIAN/control; \
+	dpkg --build content "$$deb"; \
+	mv "$$deb" "$$REPODIR/output/deb"; \
+	cat content/DEBIAN/control >> $$REPODIR/output/deb/Packages; \
+	md5=`md5sum "$$REPODIR/output/deb/$$deb" | cut -d' ' -f 1`; \
+	echo "MD5Sum: $$md5" >> $$REPODIR/output/deb/Packages; \
+	echo "" >> $$REPODIR/output/deb/Packages
 
 clean:
 	rm -rf content
