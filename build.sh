95e21b8d01030d6a6d0ff3c5e54009e8aee3c9b5
Max Neklyudov <macscomp@gmail.com>
2013-04-15 16:45:27 +0400

    Improve build script

--- build.sh
+++ build.sh
@@ -15,19 +15,13 @@ rm -rf ./romfs
 if [[ "$1" = "packages" ]]; then
 	packages="git git-svn openocd libncurses5-dev xutils-dev libgmp-dev libmpfr-dev libmpc-dev lzop u-boot-tools mtd-utils putty genext2fs cmake libglib2.0-dev libtool autoconf fakeroot"
 	$SYS_INSTALLER $packages
-	exit 0
+elif [[ "$1" = "repo" ]]; then
+	fakeroot -- make repo CROSS=arm-uclinuxeabi- CROSS_COMPILE=arm-uclinuxeabi-
+else
+	if [[ "$1" = "rebuild" ]]; then
+		make clean CROSS=arm-uclinuxeabi- CROSS_COMPILE=arm-uclinuxeabi- || exit 1
+	fi
+	make TOOLCHAIN=$TOOLCHAIN CROSS_COMPILE=arm-uclinuxeabi- tools automake subdirs linux_image || exit 1
+	fakeroot -- make TOOLCHAIN=$TOOLCHAIN CROSS_COMPILE=arm-uclinuxeabi- romfs image || exit 1
+	echo == Compiled ==
 fi
-
-if [[ "$1" = "repo" ]]; then
-	make repo CROSS=arm-uclinuxeabi- CROSS_COMPILE=arm-uclinuxeabi-
-	exit 0
-fi
-
-if [[ "$1" = "rebuild" ]]; then
-  make clean CROSS=arm-uclinuxeabi- CROSS_COMPILE=arm-uclinuxeabi- || exit 1
-fi
-
-make TOOLCHAIN=$TOOLCHAIN CROSS_COMPILE=arm-uclinuxeabi- tools automake subdirs linux_image || exit 1
-fakeroot -- make TOOLCHAIN=$TOOLCHAIN CROSS_COMPILE=arm-uclinuxeabi- romfs image || exit 1
-
-echo == Compiled ==
