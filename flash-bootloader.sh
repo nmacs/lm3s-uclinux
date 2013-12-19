ea77aaf87c622c1b208d6a56d2b7ac0f3bc700b2
Max Neklyudov <macscomp@gmail.com>
2013-12-19 11:35:27 +0400

    flash-bootloader.sh: use tools/openocd

--- flash-bootloader.sh
+++ flash-bootloader.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+
 ROOT=`pwd`
 image="$ROOT/images/uboot.img"
 rom_disk="$ROOT/images/romdisk.img"
-openocd -f "$ROOT/vendors/Elster/uwic/uwic-jtag.cfg" -c "flash_mcu $image $rom_disk" || exit 1
+tools/openocd -f "$ROOT/vendors/Elster/uwic/uwic-jtag.cfg" -c "flash_mcu $image $rom_disk" || exit 1
 
 echo "== Flashed =="
