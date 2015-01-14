62ea6fb5c9f42f65a62bedee5f17db72c7cb0a3f
Max Neklyudov <macscomp@gmail.com>
2015-01-14 12:19:47 +0300

    deploy-romfs.sh: do not deploy nfsroot

--- deploy-romfs.sh
+++ deploy-romfs.sh
@@ -10,10 +10,10 @@ cp -r $ROOT/images /nfsroot/
 cp $ROOT/romfs/boot/linux.bin /tftpboot/
 cp $ROOT/images/initrd.bin /tftpboot/
 
-mkdir -p /tmp/uwic-image
-mount $ROOT/images/Image.ext2 /tmp/uwic-image
-cp -r /tmp/uwic-image/dev /nfsroot/
-umount /tmp/uwic-image
+#mkdir -p /tmp/uwic-image
+#mount $ROOT/images/Image.ext2 /tmp/uwic-image
+#cp -r /tmp/uwic-image/dev /nfsroot/
+#umount /tmp/uwic-image
 
 cp $ROOT/java/*.zip /tftpboot/
 
